3e55653c3a504b0983c95773dc230447a75db5fd
PercyTang <Percy.Tang@infineon.com>
2022-09-01 11:16:14 +0800

    change start position

--- CustomTranslator.Win/Main.Designer.cs
+++ CustomTranslator.Win/Main.Designer.cs
@@ -39,9 +39,9 @@
             // 
             // btnSubmit
             // 
-            this.btnSubmit.Location = new System.Drawing.Point(384, 276);
+            this.btnSubmit.Location = new System.Drawing.Point(384, 244);
             this.btnSubmit.Name = "btnSubmit";
-            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.Size = new System.Drawing.Size(75, 20);
             this.btnSubmit.TabIndex = 0;
             this.btnSubmit.Text = "Submit";
             this.btnSubmit.UseVisualStyleBackColor = true;
@@ -51,41 +51,41 @@
             // 
             this.richTextBox1.Location = new System.Drawing.Point(-1, 0);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(602, 113);
+            this.richTextBox1.Size = new System.Drawing.Size(602, 100);
             this.richTextBox1.TabIndex = 1;
             this.richTextBox1.Text = "";
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(0, 150);
+            this.richTextBox2.Location = new System.Drawing.Point(0, 132);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(601, 120);
+            this.richTextBox2.Size = new System.Drawing.Size(601, 106);
             this.richTextBox2.TabIndex = 2;
             this.richTextBox2.Text = "";
             // 
             // lab1
             // 
             this.lab1.AutoSize = true;
-            this.lab1.Location = new System.Drawing.Point(54, 123);
+            this.lab1.Location = new System.Drawing.Point(54, 109);
             this.lab1.Name = "lab1";
-            this.lab1.Size = new System.Drawing.Size(49, 17);
+            this.lab1.Size = new System.Drawing.Size(45, 15);
             this.lab1.TabIndex = 3;
             this.lab1.Text = "English";
             // 
             // lab2
             // 
             this.lab2.AutoSize = true;
-            this.lab2.Location = new System.Drawing.Point(56, 276);
+            this.lab2.Location = new System.Drawing.Point(56, 244);
             this.lab2.Name = "lab2";
-            this.lab2.Size = new System.Drawing.Size(32, 17);
+            this.lab2.Size = new System.Drawing.Size(31, 15);
             this.lab2.TabIndex = 4;
             this.lab2.Text = "中文";
             // 
             // btnSwitch
             // 
-            this.btnSwitch.Location = new System.Drawing.Point(278, 276);
+            this.btnSwitch.Location = new System.Drawing.Point(278, 244);
             this.btnSwitch.Name = "btnSwitch";
-            this.btnSwitch.Size = new System.Drawing.Size(75, 23);
+            this.btnSwitch.Size = new System.Drawing.Size(75, 20);
             this.btnSwitch.TabIndex = 5;
             this.btnSwitch.Text = "Switch";
             this.btnSwitch.UseVisualStyleBackColor = true;
@@ -93,9 +93,9 @@
             // 
             // btnPasteCommit
             // 
-            this.btnPasteCommit.Location = new System.Drawing.Point(490, 276);
+            this.btnPasteCommit.Location = new System.Drawing.Point(490, 244);
             this.btnPasteCommit.Name = "btnPasteCommit";
-            this.btnPasteCommit.Size = new System.Drawing.Size(98, 23);
+            this.btnPasteCommit.Size = new System.Drawing.Size(98, 20);
             this.btnPasteCommit.TabIndex = 6;
             this.btnPasteCommit.Text = "PasteCommit";
             this.btnPasteCommit.UseVisualStyleBackColor = true;
@@ -103,9 +103,9 @@
             // 
             // Main
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 332);
+            this.ClientSize = new System.Drawing.Size(600, 293);
             this.Controls.Add(this.btnPasteCommit);
             this.Controls.Add(this.btnSwitch);
             this.Controls.Add(this.lab2);
@@ -114,6 +114,7 @@
             this.Controls.Add(this.richTextBox1);
             this.Controls.Add(this.btnSubmit);
             this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "CustomTranslator";
             this.ResumeLayout(false);
             this.PerformLayout();
