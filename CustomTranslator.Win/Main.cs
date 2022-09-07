96e33ac4faa95c2e1832c4c1ce4cafcb68128a08
xjtrab <xjtrab@gmail.com>
2022-09-07 17:05:47 +0800

    Revert "Change server ip"
    This reverts commit 86974ad48601d36976b04e3ef9134a6a76fd0167.

--- CustomTranslator.Win/Main.cs
+++ CustomTranslator.Win/Main.cs
@@ -8,7 +8,7 @@ namespace CustomTranslator.Win
     public partial class Main : Form
     {
         private static bool ChinseToEnglish = false;
-        private string url = @"http://110.42.246.176/api/TextTranslator?text={0}&from={1}&to={2}";
+        private string url = @"http://103.172.182.110/api/TextTranslator?text={0}&from={1}&to={2}";
         private KeyboardHook k_hook;
         private string currentKey = string.Empty;
         private DateTime currentTime = DateTime.UtcNow;
