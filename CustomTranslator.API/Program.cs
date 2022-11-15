61adc7531d9bfc4d6661d5a52e2b753d35f8c024
xjtrab <xjtrab@gmail.com>
2022-11-16 00:02:47 +0800

    still have bug

--- CustomTranslator.API/Program.cs
+++ CustomTranslator.API/Program.cs
@@ -6,23 +6,25 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 // Replace with your connection string.
-var connectionString = "server=localhost;user=root;password=wo113661;port=3307;database=translator";
+var connectionString = builder.Configuration.GetConnectionString("TranslatorDatabase");
 
 // Replace with your server version and type.
 // Use 'MariaDbServerVersion' for MariaDB.
 // Alternatively, use 'ServerVersion.AutoDetect(connectionString)'.
 // For common usages, see pull request #1233.
 var serverVersion = new MySqlServerVersion(new Version(5, 7, 24));
-
+Console.WriteLine(string.Format(connectionString, Environment.GetEnvironmentVariable("DATABASE_HOST")));
 builder.Services.AddDbContext<TranslatorContext>(
     dbContextOptions => dbContextOptions
-        .UseMySql(connectionString, serverVersion)
+        .UseMySql(string.Format(connectionString, Environment.GetEnvironmentVariable("DATABASE_HOST")), serverVersion)
         // The following three options help with debugging, but should
         // be changed or removed for production.
-        .LogTo(Console.WriteLine, LogLevel.Information)
+        .LogTo(Console.WriteLine, LogLevel.Debug)
         .EnableSensitiveDataLogging()
         .EnableDetailedErrors()
 );
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -30,6 +32,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+await app.Services.GetRequiredService<TranslatorContext>().Database.MigrateAsync();
 
 // Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
