3bf87fa491b1088a5c579d9fd1a5f95947eb1662
xjtrab <xjtrab@gmail.com>
2022-11-24 10:59:11 +0800

    add api get history

---
+++ CustomTranslator.API/Controllers/TranslatorHistoryController.cs
@@ -0,0 +1,22 @@
+﻿using CustomTranslator.API.DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomTranslator.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TranslatorHistoryController : ControllerBase
+    {
+        private readonly TranslatorContext translatorContext;
+        public TranslatorHistoryController(TranslatorContext translatorContext)
+        {
+            this.translatorContext = translatorContext;
+        }
+        [HttpGet]
+        public async Task<List<TranslatorHistory>> Get()
+        {
+            return translatorContext.TranslatorHistorys.ToList();
+        }
+    }
+}
